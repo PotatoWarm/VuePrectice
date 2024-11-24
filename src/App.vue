e46816883ad39f8db574df537db033b1236be9eb
胡玲 <704214661@qq.com>
2024-11-24 20:37:51 +0800

    11/24

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+      <Sightname/>
+      <hr/>
+      <Sightaddress/>
+   </div>
+</template>
+
+<script>
+import Sightname from "@/components/Sightname";
+import Sightaddress from "@/components/Sightaddress";
+export default {
+  name: "App",
+  components:{
+    Sightaddress,
+    Sightname,
+  },
+}
+</script>
+
---
+++ src/components/Sightname.vue
@@ -0,0 +1,22 @@
+<template>
+   <div >
+     <h2>姓名:{{  name }}</h2>
+     <h2>性别: {{ sex }}</h2>
+     <input v-fbind:value="name"/>
+   </div>
+</template>
+
+<script>
+
+export default {
+  name: "Sightname",
+  data(){
+    console.log(this);
+    return {    
+       name: '玲玲',
+       sex: '女',
+    }
+  },
+}
+</script>
+
