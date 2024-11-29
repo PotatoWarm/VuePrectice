14e992b1c0bf1430b96a65fb341d1e7c872d954f
胡玲 <704214661@qq.com>
2024-11-29 15:22:05 +0800

    分析ref属性

---
+++ 02_src_分析ref属性/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 v-text="msg" ref="title"></h1>
+    <button @click="showDOM">点我输出DOM元素</button>
+    <Sightadress ref="Sightadress"></Sightadress>
+  </div>
+</template>
+
+<script>
+import Sightadress from './components/Sightadress.vue'; 
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: '欢迎来到橘子洲头',
+    }
+  },
+  methods: {
+    showDOM(e) {
+      // 确保在nextTick中访问DOM
+        console.log('标题元素:', this.$refs.title);
+        console.log('点击的按钮:', e.target);
+        console.log('子组件:', this.$refs.Sightadress);
+    }
+  },
+  components: { 
+    Sightadress,
+   },
+}
+</script>     
---
+++ 02_src_分析ref属性/components/Sightadress.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="sightadress">
+    <h2>景点名称：{{ sightname }}</h2>
+    <h2>景点地址：{{ sightadress }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sightadress',
+  data() {
+    return {
+      sightname: '橘子洲头',
+      sightadress: '湖南省长沙市岳麓区橘子洲头'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sightadress {
+  background-color:deeppink;
+}
+</style>
