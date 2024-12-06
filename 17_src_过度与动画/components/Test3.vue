8ebde7b1c2e84f565832c0734ddab0b554d976f0
胡玲 <704214661@qq.com>
2024-12-06 17:17:50 +0800

    过渡与动画

---
+++ 17_src_过度与动画/components/Test3.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <button @click="isShow = !isShow">
+            显示/隐藏
+        </button>
+
+        <transition-group
+              appear
+              name="animate_animated animate_bounce"
+              enter-active-class="animate_swing"
+              leave-active-class="animate_backOutup"
+        >
+
+        <h1 v-show="isShow" key="1">你好！</h1>
+        <h1 v-show="isShow" key="2">世界！</h1>
+        
+        </transition-group>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name:"Test3",
+    data(){
+        return{
+            isShow:true
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1{
+    background: orange;
+}
+</style>
