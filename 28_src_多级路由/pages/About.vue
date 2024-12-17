c46cdcaed1d0049ce20c0d5db998b5d4bbc92cb2
胡玲 <704214661@qq.com>
2024-12-17 09:15:26 +0800

    报错问题

---
+++ 28_src_多级路由/pages/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>我是About的内容</h2>
+</template>
+
+<script>
+export default{
+    name:"About",
+    //mounted(){
+    //    console.log('Home组件挂载完毕'，this)；
+    //    window.aboutRoute = this.$route;
+    //    window.aboutRoute = this.$router;
+    //},
+    //beforeDestory(){
+    //   console.log('About组件即将被销毁')；
+    //}
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,29 @@
 const { defineConfig } = require('@vue/cli-service')
-// 使用defineConfig函数定义配置对象
+const path = require('path')
+
 module.exports = defineConfig({
-  // 使用transpileDependencies属性指定需要转译的依赖
   transpileDependencies: true,
-  // 使用lintOnSave属性指定是否在保存时检查代码
   lintOnSave: false,
-  // 使用css属性指定css的配置
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001/', // 目标 API 服务器
-        changeOrigin: true, // 是否改变源
-        pathRewrite: { '^/api': '' }, // 重写路径
+        target: 'http://localhost:5001/',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
       },
     },
   },
   css: {
-    // 使用loaderOptions属性指定less的配置
     loaderOptions: {
       less: {
-        // 使用lessOptions属性指定less的配置
         lessOptions: {
-          // 使用javascriptEnabled属性指定是否启用javascript
           javascriptEnabled: true
         }
       }
     }
+  },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
   }
 })
